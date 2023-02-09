523fb49134be6812a8b4863fd525dd4e99b74b1e
Amari-17 <124882315+Amari-17@users.noreply.github.com>
2023-02-10 00:19:11 +0530

    Update detail_collector.py

--- detail_collector.py
+++ detail_collector.py
@@ -9,16 +9,16 @@ class DetailCollector(object):
         self.contacts = self.get_contacts()
 
     def get_skills(self):           # gets skills as a list
-        with open("/static/data/data_skills.txt") as skills_data:
+        with open("static/data/data_skills.txt") as skills_data:
             skills = skills_data.readlines()
         return skills
 
     def get_projects(self):         # gets projects as a json
-        with open("/static/data/data_projects.json") as projects_data:
+        with open("static/data/data_projects.json") as projects_data:
             projects = json.load(projects_data)
         return projects['projects']
 
     def get_contacts(self):         # gets contacts as a json
-        with open("/static/data/data_contacts.json") as contacts_data:
+        with open("static/data/data_contacts.json") as contacts_data:
             contacts = json.load(contacts_data)
         return contacts['contacts']
